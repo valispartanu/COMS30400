a38678aff87ab4d264d865eb5beda9b92cdc6396
Dan <dan.weaver@hotmail.co.uk>
2021-03-23 16:00:35 +0000

    Overlays: remove overlays for all players

--- Freight/Assets/Scripts/Photon/Fence/BreakFencePhoton.cs
+++ Freight/Assets/Scripts/Photon/Fence/BreakFencePhoton.cs
@@ -76,7 +76,7 @@ public class BreakFencePhoton : MonoBehaviourPun
                 {
                     Vector3 spawnPosition = transform.position;
                     
-                    photonView.RPC("SetPressPToNotActive", player.GetComponent<PhotonView>().Owner);
+                    photonView.RPC(nameof(SetPressPToNotActive),  RpcTarget.All);
 
                     photonView.RPC("DestroyFence", RpcTarget.MasterClient);
                     PhotonNetwork.Instantiate("PhotonPrefabs/fence_simple_broken_open Variant 1", spawnPosition, Quaternion.Euler(0f, 90f, 0f));
